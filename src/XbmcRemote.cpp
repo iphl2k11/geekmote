59194237bdedf37bb02f35df5066639034679800
Filip Franczyk <filip.franczyk@gmail.com>
2013-10-21 20:31:16 +0200

    initial commit

---
+++ src/XbmcRemote.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : XbmcRemote.cpp
+// Author      : Filip Franczyk
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ test/SimpleTest.cpp
@@ -0,0 +1,19 @@
+/*
+ * SimpleTest.cpp
+ *
+ *  Created on: Oct 17, 2013
+ *      Author: filipfranczyk
+ */
+
+#include <gtest/gtest.h>
+using ::testing::Test;
+
+TEST(SimpleTests, Nothing)
+{
+	ASSERT_TRUE(true);
+}
+
+TEST(SimpleTests, Nothing2)
+{
+	ASSERT_TRUE(true);
+}
