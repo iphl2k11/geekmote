bda39d9e27237b6645d912a1b0ea5b304b0ee2fb
Filip Franczyk <filip.franczyk@gmail.com>
2013-10-21 21:00:36 +0200

    Main class object added.

---
+++ Main/Main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Oct 21, 2013
+ *      Author: filipfranczyk
+ */
+#include "Main.h"
+
+void Main::Execute(int argc, char** argv)
+{
+
+}
+
+
---
+++ Main/Main.h
@@ -0,0 +1,19 @@
+/*
+ * Main.h
+ *
+ *  Created on: Oct 21, 2013
+ *      Author: filipfranczyk
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+//TODO  make it bost noncopyable
+//
+class Main
+{
+	void Execute(int argc, char** argv);
+};
+
+
+#endif /* MAIN_H_ */
