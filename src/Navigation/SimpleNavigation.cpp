cddb8d05357de21bf855a3218a02040438b65125
Filip Franczyk <filip.franczyk@gmail.com>
2013-10-26 16:32:28 +0200

    adding untracked files

---
+++ src/Navigation/SimpleNavigation.cpp
@@ -0,0 +1,37 @@
+/*
+ * SimpleNavigation.cpp
+ *
+ *  Created on: Oct 21, 2013
+ *      Author: filipfranczyk
+ */
+
+#include "SimpleNavigation.h"
+
+SimpleNavigation::SimpleNavigation() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SimpleNavigation::~SimpleNavigation() {
+	// TODO Auto-generated destructor stub
+}
+
+std::string SimpleNavigation::NowPlaying()
+{
+	return "";
+}
+
+void SimpleNavigation::Play()
+{
+
+}
+
+void SimpleNavigation::Pause()
+{
+
+}
+
+void SimpleNavigation::Stop()
+{
+
+}
